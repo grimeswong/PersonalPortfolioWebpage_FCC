481c5f14442cf8b25dcc8f5299a204c7935bd991
Shadow Grimes <grimeswongatwork@gmail.com>
2019-05-07 00:59:23 +1000

    ppw-004: rotate the navbar background colours & change the projects background colour
    change the navbar colour depends on the scrolling sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 /**
   * This is the main JavaScript file
   */
+
+$(document).ready(function() {
+  // if (typeof jQuery != 'undefined') {
+  //   // jQuery is loaded => print the version
+  //   // alert($().jquery);
+  //   console.log("Jquery version = " + $().jquery);
+  // }
+
+  $(window).scroll(function() {
+    // console.log("Current Top = "+ $(window).scrollTop());
+    // console.log("Welcome Top = " + $("#welcome-section").offset().top);
+    // console.log("Project Top = " + $("#projects").offset().top);
+    // console.log("Navbar height = " + $("#navbar").height());
+    if ($(window).scrollTop() == $("#welcome-section").offset().top || $(window).scrollTop() <= $("#projects").offset().top - $("#navbar").height()) {
+      $("#navbar").addClass("navbar-welcome");
+      $("#navbar").removeClass("navbar-projects");
+    } else if ($(window).scrollTop() >= $("#projects").offset().top ) {
+      $("#navbar").addClass("navbar-projects");
+      $("#navbar").removeClass("navbar-welcome");
+    }
+  });
+
+});
